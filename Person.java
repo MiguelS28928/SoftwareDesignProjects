03870459c399b90ce0efc9b8c618105d19f1b80f
unknown <miguelpaologs0418@gmail.com>
2021-03-11 21:10:21 +0800

    second commit

--- Person.java
+++ Person.java
@@ -1,1 +1,7 @@
-Miguel Paolo Gerard H. Salarda
+public class assignment004
+
+{
+        public static void main(String[] args) {
+                System.out.println("Miguel Paolo Gerard H. Salarda");
+        }
+}
