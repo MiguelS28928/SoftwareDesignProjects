5eb5d59541d8bc79c6f4ca307c4b34148c1c23e0
unknown <miguelpaologs0418@gmail.com>
2021-03-11 21:17:20 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class assignment004
+{
+public static void main(String[] args)
+{
+System.out.println("Miguel Paolo Gerard H. Salarda");
+System.out.println("CJ Dasal");
+System.out.println("Winly Alcala");
+System.out.println("Jan Gennai");
+System.out.println("Yoshio Alotaya");
+}
+}
